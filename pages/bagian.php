ae14e5d93d679f0bd870583bfd01fd98e3ad697b
agustiarfajar <fajar.kdt03@gmail.com>
2022-08-03 09:33:56 +0700

    selesai

--- pages/bagian.php
+++ pages/bagian.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION["kode_user"]))
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Data Pengguna Sistem</h3>
+                    <h3>Data Bagian</h3>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
